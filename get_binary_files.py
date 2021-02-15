39608b1676cf7df88e500008abdc208379dd280b
Nothanks <andreas@localhost.localdomain>
2021-02-15 18:31:23 +0000

    Binary_files

--- get_binary_files.py
+++ get_binary_files.py
@@ -3,13 +3,18 @@ packets = pyshark.FileCapture(
             input_file="capture1.pcap",
             use_json=True,
             include_raw=True
-          )._packets_from_tshark_sync() # pcap_dir is the directory of my pcap file
+          )
 
-hex_packet = packets.__next__().frame_raw.value
-print(hex_packet)
-
-binary_packet = bytearray.fromhex(hex_packet)
-print(binary_packet)
-f = open('packet1.bin', 'w+b')
-f.write(binary_format)
-f.close()
+counter = 1
+length = len(packets)
+print(length)
+for packet in packets:
+	hex_packet = packet.frame_raw.value
+	print(hex_packet)
+	binary_packet = bytearray.fromhex(hex_packet)
+	#print(binary_packet)
+	name = "binary_files/" + str(counter) + ".bin"
+	f = open(name, 'w+b')
+	f.write(binary_packet)
+	f.close()
+	counter += 1
